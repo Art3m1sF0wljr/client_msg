e47f19612854e6f80720891894e559af910e6b56
dcazacu <158522530+Art3m1sF0wljr@users.noreply.github.com>
2025-02-08 13:29:12 +0100

    Update client.py

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ from Crypto.Hash import SHA256
 from websocket import create_connection
 
 # === Configuration ===
-SERVER_IP_URL = "enlighttheworld.altervista.org/ip/ip.txt"  # Placeholder for actual server IP source
+SERVER_IP_URL = "https://enlighttheworld.altervista.org/ip/ip.txt"  # Placeholder for actual server IP source
 SERVER_PORT = 5002
 LOCAL_DATA_FILE = "client_data.json"
 
